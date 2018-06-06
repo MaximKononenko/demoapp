8563679767e87c3bb6c6522a755a5f4e46f01e9d
MaximKononenko <kononenkomi@gmail.com>
2018-06-06 14:47:08 +0300

    Update pack.sh

--- infr/pack.sh
+++ infr/pack.sh
@@ -6,11 +6,19 @@ tmpLog="tmp.log"
 cd app
 
 cat >> "Dockerfile" << EOF
-FROM nginx:latest
-RUN apt-get update
-RUN apt-get install -y curl
-COPY . /usr/share/nginx/html
-EXPOSE 80
+FROM andreptb/maven:latest as BUILD
+WORKDIR /usr/src/
+COPY . .
+RUN ls -la /usr/src/initial
+RUN cd initial && mvn package
+RUN ls -la /usr/src/initial/target
+
+# STAGE 2 - Pack container
+FROM tomcat:alpine
+ENV VERSION 0.0.1
+COPY --from=BUILD /usr/src/initial/target/gs-spring-boot-0.1.0.jar /usr/local/tomcat/webapps/
+EXPOSE 8080/tcp
+CMD ["catalina.sh", "run"]
 EOF
 
 ecrlogin=$(~/.local/bin/aws ecr get-login --no-include-email --region us-east-1)
